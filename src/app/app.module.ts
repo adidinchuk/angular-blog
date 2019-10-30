4c85ab599c696818944caff64a78b95c55aebffc
adidinchuk <adidinchuk@gmail.com>
2019-10-30 00:18:49 -0400

    digit front end /w docker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigitsComponent } from './components/digits/digits.component';
+import { CanvasComponent } from './components/digits/canvas/canvas.component';
+import { DigitControlComponent } from './components/digits/digit-control/digit-control.component';
+import { PredictionComponent } from './components/digits/prediction/prediction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitsComponent,
+    CanvasComponent,
+    DigitControlComponent,
+    PredictionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
